ec92802293187d66cd2bc9cb2020a9a92cf93f43
maatcha <mattcha54@gmail.com>
2018-07-27 00:45:23 +0200

    Finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+10.times do
+	pa = Part.create(part_number: Faker::Number.between(1, 10))
+end
+
+10.times do
+	a = Assembly.create(name: Faker::Music.key)
+	a.parts << pa
+end
+
